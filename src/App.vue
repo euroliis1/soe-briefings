86d7d44000d2847d8adc215bd5a283f15254baef
euroliis1 <0euroliis0@gmail.com>
2025-01-17 20:32:09 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -89,15 +89,20 @@ export default {
 
   data() {
     return {
-      "mission_slug": "001",
+      "mission_slug": "002",
       "current_md": "",
       "events": "",
       "missions": [
         {
           "slug": "001",
           "name": "Escort Job",
-          "status": "start"
+          "status": "success"
         },
+        {
+          "slug": "002",
+          "name" "Secure the Operative",
+          "status": "start"
+        }
       ],
       "pilots": [
         {
